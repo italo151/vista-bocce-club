f61337936f436085097dce9cd9db3c498843c9b4
italo151 <blasighb@yahoo.com>
2026-08-25 15:45:51 -0700

    Launch Vista Bocce Club Version 2

---
+++ content/site-data.js
@@ -0,0 +1,8 @@
+window.SITE_DATA={
+events:[
+{dateTop:"Coming",dateBottom:"Soon",title:"Vista Bocce Club Open Play",details:"Brengle Terrace Park · Vista, CA",tag:"Community"},
+{dateTop:"Coming",dateBottom:"Soon",title:"Learn to Play Bocce",details:"Beginner-friendly instruction + social play",tag:"Beginner"},
+{dateTop:"Future",dateBottom:"Event",title:"Vista Bocce Tournament",details:"Tournament details and registration will be announced here",tag:"Tournament"}
+],
+sponsors:["Founding Sponsor Available","Community Partner Available","Court Sponsor Available","Event Sponsor Available"]
+};
